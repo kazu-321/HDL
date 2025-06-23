b09c958eac60265ea22fe8de5e58c8e126b15e34
kazu-321 <kzs321kzs@gmail.com>
2025-06-23 10:20:16 +0900

    add md

--- 08/kadai5/02/circuit.v
+++ 08/kadai5/02/circuit.v
@@ -3,9 +3,11 @@ module circuit (
     output z
 );
 
-wire w1, w2, w3;
-nor nor1(w1,x1,x2);
-and and1(w2,x1,w1);
-and and2(w3,x2,w1);
-or or1(z,w2,w3);
+wire w1, w2, w3, w4, w5;
+not not1(w1,x1);
+not not2(w2,x2);
+or or1(w3,w1,w2);
+and and1(w4,x1,w3);
+and and2(w5,x2,w3);
+or or2(z,w4,w5);
 endmodule
