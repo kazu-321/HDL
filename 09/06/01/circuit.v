ca8b8ef75af2d34894d2b18b3d2f093deba8680a
kazu-321 <kzs321kzs@gmail.com>
2025-06-30 10:35:25 +0900

    9-6追加

---
+++ 09/06/01/circuit.v
@@ -0,0 +1,9 @@
+module kadai09_6 (
+    input [1:0] a,
+    input [1:0] b,
+    output [3:0] z
+);
+
+    assign z = {2'b00, a} + {2'b00, b};
+
+endmodule
---
+++ 09/06/01/kadai09_6_test.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ns
+module kadai09_6_test;
+reg [1:0] a,b;
+wire [3:0] z;
+
+kadai09_6 k(a,b,z);
+
+initial begin
+    $dumpfile("kadai09_6_test.vcd");
+    $dumpvars(0,kadai09_6_test);
+    $monitor("%t: a = %b, b = %b, z = %b", $time, a, b, z);
+        a=2'b00; b=2'b00;
+    #10 a=2'b01;
+    #10 a=2'b10;
+    #10 a=2'b11;
+    #10 a=2'b00; b=2'b01;
+    #10 a=2'b01;
+    #10 a=2'b10;
+    #10 a=2'b11;
+    #10 a=2'b00; b=2'b10;
+    #10 a=2'b01;
+    #10 a=2'b10;
+    #10 a=2'b11;
+    #10 a=2'b00; b=2'b11;
+    #10 a=2'b01;
+    #10 a=2'b10;
+    #10 a=2'b11;
+    #10 $finish;
+end
+endmodule
